ab9e1938b07cd10b9a61775d5f14bdf9f3c37a5b
lincoffeebean <reeyanlin@gmail.com>
2025-11-07 20:39:56 -0500

    Refactor background.js to add installation listener and improve side panel opening logic

--- UI/background.js
+++ UI/background.js
@@ -1,5 +1,7 @@
-chrome.sidePanel.setOptions({ path: "sidepanel.html", enabled: true });
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed and background service worker loaded.");
+});
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.sidePanel.open({ windowId: tab.windowId });
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.sidePanel.open({ windowId: tab.windowId });
 });
